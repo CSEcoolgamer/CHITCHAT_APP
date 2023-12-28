28c3eb1f423b26914cb58d66c9debeaa270cf752
SACHIN NIRATE <96587569+CSEcoolgamer@users.noreply.github.com>
2023-12-28 23:42:10 +0530

    basic login page

---
+++ src/components/Login.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { GoogleOutlined, FacebookOutlined } from "@ant-design/icons";
+
+const Login = () => {
+    return (
+        <div id="login-page">
+            <div id="login-cart">
+                <h2>Welcome To Chitchat!</h2>
+
+                <div
+                    className="login-button google"
+                >
+                    <GoogleOutlined/> Sign in with Google
+                </div>
+
+                <br/> <br/>
+
+                <div
+                    className="login-button facebook"
+                >
+                    <FacebookOutlined /> Sign in with Facebook
+
+                </div>
+
+            </div>
+
+        </div>
+    );
+
+}
+
+export default Login;
